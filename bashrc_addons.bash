180e70b5d2eed58e5ac016a7234eaee9176335e0
Patrick Winterhalder <64912762+patrickw135@users.noreply.github.com>
2020-11-04 11:49:42 +0100

    Update bashrc_addons.bash

--- bashrc_addons.bash
+++ bashrc_addons.bash
@@ -10,17 +10,16 @@ echo "ROS 2 Setup:"
 
 # Source Underlay
 source /opt/ros/foxy/setup.bash
-
 #Print Underlay
 echo "ROS Underlay:       source /opt/ros/foxy/setup.bash"
 
-# Source all workspace overlays:
-source ~/ws_overlay_foxy/install/local_setup.bash
+# Source all Overlays:
+#source ~/ws_overlay_foxy/install/local_setup.bash
 source ~/colcon_ws/install/local_setup.bash
 
 #Print Overlays
 echo "ROS Overlay:        source <ws>/install/local_setup.bash"
-echo "Overlays:           'ws_overlay_foxy' ,  'colcon_ws'"
+echo "Overlays:           'colcon_ws'"
 echo "******************************************************"
 
 # ROS 2 Settings
@@ -41,10 +40,16 @@ echo "******************************************************"
 
 # Print DDS Settings
 echo "ROS 2 DDS Settings:"
+export ROS_DOMAIN_ID=69
 echo "ROS_DOMAIN_ID:      "$ROS_DOMAIN_ID
 echo "To change:          sudo nano /opt/ros/foxy/setup.bash"
 echo "******************************************************"
 
+# Source colcon directory jump:
+source /usr/share/colcon_cd/function/colcon_cd.sh
+export _colcon_cd_root=~/colcon_ws
+
+# Print active Node & Topics
 echo "ROS 2 Topics active:"
 ros2 topic list
 echo "******************************************************"
