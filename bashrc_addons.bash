466078769e8cd1072d7691399dcef9ec8b81b461
Patrick Winterhalder <64912762+patrickw135@users.noreply.github.com>
2020-11-24 11:32:26 +0100

    Update bashrc_addons.bash

--- bashrc_addons.bash
+++ bashrc_addons.bash
@@ -1,25 +1,34 @@
 # Add these lines to .bashrc, do this:
-# sudo nano ~/.bashrc
-# move to end of .bashrc (arrow down)
-# Highlight the lines below and copy
-# Paste into console at the end of .bashrc
-# Console pasting: CTRL+LShift+V
+# Add the the following in between "#----" to the end of your .bashrc script
 
+# To do this open .bashrc:
+# sudo nano ~/.bashrc
+
+# Move to end of .bashrc (arrow down)
+# Highlight the lines below and copy (Ctrl+C)
+# Paste into console at the end of .bashrc (Right click, paste or CTRL+LShift+V)
+
+
+
+#----------------------------------------------------------------------------------
 # ROS 2 SETUP
 echo "ROS 2 Setup:"
 
 # Source Underlay
-source /opt/ros/foxy/setup.bash
-#Print Underlay
-echo "ROS Underlay:       source /opt/ros/foxy/setup.bash"
+cmd="/opt/ros/foxy/setup.bash"
+source $cmd
+echo "ROS Underlay:       "$cmd
 
-# Source all Overlays:
-#source ~/ws_overlay_foxy/install/local_setup.bash
-source ~/colcon_ws/install/local_setup.bash
+# Source Overlay
+# Change to the path of your workspace's "install/local_setup.bash" file
+cmd="$HOME/colcon_ws/install/local_setup.bash"
+source $cmd
+echo "ROS Overlay:        "$cmd
 
-#Print Overlays
-echo "ROS Overlay:        source <ws>/install/local_setup.bash"
-echo "Overlays:           'colcon_ws'"
+# You can add other workspaces
+#cmd="$HOME/colcon_libs/install/local_setup.bash"
+#source $cmd
+#echo "ROS Overlay:        "$cmd
 echo "******************************************************"
 
 # ROS 2 Settings
@@ -31,12 +40,13 @@ echo "ROS_DISTRO:         "$ROS_DISTRO
 echo "******************************************************"
 
 # Define DDS Channel
-# (this is the channel your ROS system communicates at over your network)
-# For this, open the ROS Underlay setup file:
+# (this is the channel your ROS system communicates at over your local network)
+# To change the channel open the ROS Underlay setup file:
 # sudo nano /opt/ros/foxy/setup.bash
 # Here, add the following command
-# export ROS_DOMAIN_ID=<your_channel>
+# export ROS_DOMAIN_ID=<your_channel_nr>
 # eg.: export ROS_DOMAIN_ID=69
+# Important: no spaces before and after "=" (in bash)
 
 # Print DDS Settings
 echo "ROS 2 DDS Settings:"
@@ -46,8 +56,8 @@ echo "To change:          sudo nano /opt/ros/foxy/setup.bash"
 echo "******************************************************"
 
 # Source colcon directory jump:
-source /usr/share/colcon_cd/function/colcon_cd.sh
-export _colcon_cd_root=~/colcon_ws
+#source /usr/share/colcon_cd/function/colcon_cd.sh
+#export _colcon_cd_root=~/colcon_ws
 
 # Print active Node & Topics
 echo "ROS 2 Topics active:"
@@ -57,3 +67,7 @@ echo "******************************************************"
 #ros2 node list
 echo "******************************************************"
 printf "\n"
+echo "If your package is not listed (ros2 pkg list):"
+echo " * make sure you are sourcing the correct workspace: .bashrc"
+echo " * delete build/, install/ and rebuild w/o errors"
+#----------------------------------------------------------------------------------
